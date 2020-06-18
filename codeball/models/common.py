32738f9d72bf23166f5ffb2eca2be0acfcd62e23
Bruno <bruno@metrica-sports.com>
2020-06-17 23:26:11 -0300

    Restructure package folders and added initial dataclasses

---
+++ codeball/models/common.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import Optional, List, Dict
+
+@dataclass
+class Coordinate:
+    x: float
+    y: float
+
+@dataclass
+class Visualization:
+    start_time: int
+    end_time: int
+    players: List[str]
+    tool_id: str
+    options: Dict
+
+@dataclass
+class PatternEvent:
+    pattern: str
+    start_time: int
+    event_time: int
+    end_time: int
+    coordinates: List[Coordinate]
+    visualizations: List[Visualization]
+    tags: List[str]
+
+@dataclass
+class Pattern:
+    name: str
+    code: str
+    in_time: int = 0
+    out_time: int = 0
+    events: List[PatternEvent]
+
--- run_codeball.py
+++ run_codeball.py
@@ -1,4 +1,36 @@
 import codeball.commons.io as io
+from kloppy import (
+    load_metrica_tracking_data, 
+    load_tracab_tracking_data,
+    load_epts_tracking_data, 
+    load_statsbomb_event_data,
+    to_pandas, 
+    transform
+)
 
-fifa_xml_file ='c:/1_METRICA/1_ANALYSIS/sample-data/data/Sample_Game_3/xml_file.xml'
-io.get_dict_from_xml(fifa_xml_file)
+# from kloppy import epts_load_meta_data, epts_read_raw_data
+
+#  Sample game 3
+xml_file ='c:/1_METRICA/1_ANALYSIS/sample-data/data/Sample_Game_3/xml_file.xml'
+txt_file ='c:/1_METRICA/1_ANALYSIS/sample-data/data/Sample_Game_3/txt_file.txt'
+
+# # Kloppy
+# xml_file = 'C:/1_METRICA/1_ANALYSIS/kloppy/examples/epts/epts_meta.xml'
+# txt_file = 'C:/1_METRICA/1_ANALYSIS/kloppy/examples/epts/epts_raw.txt' 
+
+# data = io.get_dict_from_xml(xml_file)`
+dataset = load_epts_tracking_data(xml_file, txt_file)
+
+print(dataset)
+# # step 1: load metadata
+# with open(xml_file, "rb") as meta_fp:
+#     meta_data = epts_load_meta_data(meta_fp)
+
+#  # step 5: put the records in a pandas dataframe
+# with open(txt_file, "rb") as raw_fp:
+#     # we are only interested in the data from the 'heartbeat' sensor
+#     records = epts_read_raw_data(raw_fp, meta_data, sensor_ids=["position"])
+#     data_frame = DataFrame.from_records(records)
+    
+# # Pfieh.. data. That's better :-)
+#     print(data_frame.head())
