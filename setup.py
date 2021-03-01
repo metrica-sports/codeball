f9b5d4b18c67b56d6c3b07c936d3632b50afcf63
Bruno <bruno@metrica-sports.com>
2021-03-01 14:40:02 -0300

    version to v0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="codeball",
-    version="v0.1.2",
+    version="v0.1.3",
     author="Bruno Dagnino",
     author_email="bruno@metrica-sports.com",
     url="https://github.com/metrica-sports/codeball",
